6b943813bcb4b715f0b5d0323187044c8c9ecb85
Scott Vesely <svesely@apple.com>
2015-01-27 21:29:26 -0800

    - ast

--- ch5/parsing.c
+++ ch5/parsing.c
@@ -7,14 +7,39 @@
 char* answer = "Thanks for saying \"%s\"\n\n";
 
 int main( int argc, char** argv ) {
-  puts( "Lispy Version 0.0.0.0.1" );
+  mpc_parser_t* Number = mpc_new( "number" );
+  mpc_parser_t* Operator = mpc_new( "operator" );
+  mpc_parser_t* Expr = mpc_new( "expr" );
+  mpc_parser_t* Lispy = mpc_new( "lispy" );
+
+  mpca_lang( MPCA_LANG_DEFAULT,
+      "                                                   \
+      number   : /-?[0-9]+/ ;                             \
+      operator : '+' | '-' | '*' | '/' ;                  \
+      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+      lispy    : /^/ <operator> <expr>+ /$/ ;             \
+      ",
+      Number, Operator, Expr, Lispy);
+
+
+    puts( "Lispy Version 0.0.0.0.1" );
   puts( "Press C-c to Exit\n");
 
   while( 1 ) {
     char* input = readline( "vesopolisp> ");
     add_history( input );
-    printf( answer , input );
+
+    mpc_result_t r;
+    if ( mpc_parse( "<stdin>", input, Lispy, &r )) {
+      mpc_ast_print( r.output );
+      mpc_ast_delete( r.output );
+    }else{
+      mpc_err_print( r.error );
+      mpc_err_delete( r.error );
+    }
+
     free( input );
   }
+  mpc_cleanup( 4, Number, Operator, Expr, Lispy );
   return 0;
 }
