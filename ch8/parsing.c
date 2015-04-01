c5c17e1f6d24b86841707b2e624c3112bc7fdd31
Scott Vesely <svesely@apple.com>
2015-03-31 19:57:02 -0700

    - adding sexpr

--- ch8/parsing.c
+++ ch8/parsing.c
@@ -36,21 +36,23 @@ long eval( mpc_ast_t* t) {
 
 int main( int argc, char** argv ) {
   mpc_parser_t* Number = mpc_new( "number" );
-  mpc_parser_t* Operator = mpc_new( "operator" );
+  mpc_parser_t* Symbol = mpc_new( "symbol" );
+  mpc_parser_t* Sexpr = mpc_new( "sexpr" );
   mpc_parser_t* Expr = mpc_new( "expr" );
   mpc_parser_t* Lispy = mpc_new( "lispy" );
 
   mpca_lang( MPCA_LANG_DEFAULT,
       "                                                   \
       number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' | '%' ;            \
-      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-      lispy    : /^/ <operator> <expr>+ /$/ ;             \
+      symbol   : '+' | '-' | '*' | '/' ;                  \
+      sexpr    : '(' <expr>* ')' ;                        \
+      expr     : <number> | <symbol> | <sexpr> ;          \
+      lispy    : /^/ <expr>* /$/ ;                        \
       ",
-      Number, Operator, Expr, Lispy);
+      Number, Symbol, Sexpr, Expr, Lispy);
 
 
-  puts( "Lispy Version 0.0.0.0.3" );
+  puts( "Lispy Version 0.0.0.0.8" );
   puts( "Press C-c to Exit\n");
 
   while( 1 ) {
@@ -69,6 +71,6 @@ int main( int argc, char** argv ) {
 
     free( input );
   }
-  mpc_cleanup( 4, Number, Operator, Expr, Lispy );
+  mpc_cleanup( 5, Number, Symbol, Sexpr, Expr, Lispy );
   return 0;
 }
